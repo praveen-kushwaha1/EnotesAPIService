e481d93516af2c7bcdfba0a4114d2e1df7a93bbb
PRAVEEN KUSHWAHA <praveenkushwaha.tech@gmail.com>
2025-04-24 18:14:25 +0530

    enote-6-implement-auditing-category

--- src/main/java/com/praveen/config/ProjectConfig.java
+++ src/main/java/com/praveen/config/ProjectConfig.java
@@ -3,6 +3,7 @@ package com.praveen.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 
 @Configuration
 public class ProjectConfig {
@@ -11,5 +12,10 @@ public class ProjectConfig {
     ModelMapper mapper() {
 		return new ModelMapper();
 	}
+
+    @Bean
+    public AuditorAware<Integer> auditAware() {
+        return new AuditAwareConfig();
+    }
 
 }
--- src/main/java/com/praveen/entity/Category.java
+++ src/main/java/com/praveen/entity/Category.java
@@ -1,19 +1,18 @@
 package com.praveen.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Category extends BaseModel {
 
 	@Id
@@ -23,5 +22,9 @@ public class Category extends BaseModel {
 	private String name;
 
 	private String description;
+
+	private Boolean isActive;
+
+	private Boolean isDeleted;
 	
 }
